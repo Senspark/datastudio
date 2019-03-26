030970c92b527b2233285c5b5fb5d1ac5be4de39
QuangLinh <yeutrang51@gmail.com>
2019-03-26 16:26:47 +0700

    add android publisher

--- model/AndroidPublisher.js
+++ model/AndroidPublisher.js
@@ -1,190 +1,191 @@
-const EE = require('../EE');
-const ee = new EE();
-var LOCK_DURATION = 60 * 1000;
+const fetch = require('node-fetch');
 const AsyncLock = require('async-lock');
-var lock = new AsyncLock({
-    timeout: LOCK_DURATION
-});
+const AdmZip = require('adm-zip');
+const EE = require('../EE');
 
-var LOCK_DURATION = 60 * 1000;
+const LOCK_DURATION = 60 * 1000;
+const CACHE_DURATION = 6 * 60 * 60;
 
-var CONFIG_BUCKET_ID = 'BUCKET_ID';
-var DIMENSION_DATE = 'date';
-var DIMENSION_TRANSACTION_TYPE = 'type';
-var DIMENSION_PRODUCT_TITLE = 'product_title';
-var DIMENSION_PRODUCT_ID = 'product_id';
-var DIMENSION_PRODUCT_TYPE = 'product_type';
-var DIMENSION_SKU_ID = 'sku';
-var DIMENSION_BUYER_COUNTRY = 'country_iso_code';
-var METRIC_AMOUNT = 'amount';
-var _sessionId = ee.getSessionId();
+const DIMENSION_DATE = 'date';
+const DIMENSION_TRANSACTION_TYPE = 'type';
+const DIMENSION_PRODUCT_TITLE = 'product_title';
+const DIMENSION_PRODUCT_ID = 'product_id';
+const DIMENSION_PRODUCT_TYPE = 'product_type';
+const DIMENSION_SKU_ID = 'sku';
+const DIMENSION_BUYER_COUNTRY = 'country_iso_code';
+const METRIC_AMOUNT = 'amount';
 
 class AndroidPublisher {
-    getAccessToken() {
-        // fix me
-        // return 'ya29.GlwHBYfPbpTWaKXl3KbgdpUZf9SWj-d3qvts3Uf57qYmdMjjO5iqPP2t-JB1S0fOpnU1JdJGO-ZiBogA6YIedieICFhyJpf57SU-FA0H9LPFaAFBV0LGI-6y-7lzyw';
-        //return getOAuthService().getAccessToken();
+  static createOptionsFromToken(accessToken) {
+    const options = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    return options;
+  }
+
+  static createOption(token) {
+    return this.createOptionsFromToken(token);
+  }
+
+  static createZipOption(token) {
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/zip',
+      },
+    };
+    // console.log(options);
+    return options;
+  }
+
+  static mapHeaderIndex(name) {
+    if (name === DIMENSION_DATE) {
+      return 1;
     }
-    createOptionsFromToken(accessToken) {
-        var options = {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
+    if (name === DIMENSION_TRANSACTION_TYPE) {
+      return 3;
+    }
+    if (name === DIMENSION_PRODUCT_TITLE) {
+      return 5;
+    }
+    if (name === DIMENSION_PRODUCT_ID) {
+      return 6;
+    }
+    if (name === DIMENSION_PRODUCT_TYPE) {
+      return 7;
+    }
+    if (name === DIMENSION_SKU_ID) {
+      return 8;
+    }
+    if (name === METRIC_AMOUNT) {
+      return 12;
+    }
+    if (name === DIMENSION_BUYER_COUNTRY) {
+      return 16;
+    }
+    return name;
+  }
+
+  static formatObjectsListUrl(bucketId) {
+    const format = [
+      'https://www.googleapis.com/storage/v1',
+      `/b/${bucketId}`,
+      '/o',
+      '?prefix=sales',
+      '&fields=items(name,mediaLink)',
+    ].join('');
+    // console.log(format);
+    return format;
+  }
+
+  static parseReportDateRange(name) {
+    // name format: earnings/earnings_yyyyMM_xxxxxxxxxxxxxxxx-n.zip
+    // name format: sales/salesreport_yyyyMM.zip
+    const year = parseInt(name.substring(18, 22), 10);
+    const month = parseInt(name.substring(22, 24), 10);
+    const since = new Date(Date.UTC(year, month - 1, 1));
+    const until = new Date(Date.UTC(year, month, 0));
+    return {
+      since,
+      until,
+    };
+  }
+
+  static async parseData(token, data, headers, since, until) {
+    const rows = [];
+    // const parseReportDateRange  = this.parseReportDateRange;
+    if (data.items) {
+      const { items } = data;
+      await Promise.all(items.map(async (item) => {
+        const reportRange = this.parseReportDateRange(item.name);
+        const reportSince = reportRange.since;
+        const reportUntil = reportRange.until;
+        if (!(since <= reportUntil && reportSince <= until)) {
+          return;
+        }
+        const option = this.createZipOption(token);
+        const res = await fetch(item.mediaLink, option);
+        const buffer = await res.buffer();
+        const admzip = new AdmZip(buffer);
+        const zipEntries = admzip.getEntries();
+        const unzipData = admzip.readAsText(zipEntries[0]);
+        const dataRows = await EE.csvToJson(unzipData);
+        dataRows.forEach(async (row) => {
+          const date = new Date(row[this.mapHeaderIndex(DIMENSION_DATE)]);
+          if (!(since <= date && date <= until)) {
+            // Out of range.
+            return;
+          }
+          const dict = {};
+          headers.map(async (header) => {
+            let value = row[this.mapHeaderIndex(header)];
+            if (header === DIMENSION_DATE) {
+              value = EE.parseDate(value);
             }
-        };
-        return options;
-    }
-
-    createOption(accessToken) {
-        return this.createOptionsFromToken(accessToken);
-    }
-
-    mapHeaderIndex(name) {
-        if (name == DIMENSION_DATE) {
-            return 1;
-        }
-        if (name == DIMENSION_TRANSACTION_TYPE) {
-            return 3;
-        }
-        if (name == DIMENSION_PRODUCT_TITLE) {
-            return 5;
-        }
-        if (name == DIMENSION_PRODUCT_ID) {
-            return 6;
-        }
-        if (name == DIMENSION_PRODUCT_TYPE) {
-            return 7;
-        }
-        if (name == DIMENSION_SKU_ID) {
-            return 8;
-        }
-        if (name == METRIC_AMOUNT) {
-            return 12;
-        }
-        if (name == DIMENSION_BUYER_COUNTRY) {
-            return 16;
-        }
-        return name;
-    }
-
-    formatObjectsListUrl(bucketId) {
-        var format = [
-            `https://www.googleapis.com/storage/v1`,
-            `/b/${bucketId}`,
-            `/o`,
-            `?prefix=sales`,
-            `&fields=items(name,mediaLink)`
-        ].join('');
-        return format;
-    }
-
-    parseReportDateRange(name) {
-        // name format: earnings/earnings_yyyyMM_xxxxxxxxxxxxxxxx-n.zip
-        // name format: sales/salesreport_yyyyMM.zip
-        var year = parseInt(name.substring(18, 22), 10);
-        var month = parseInt(name.substring(22, 24), 10);
-        var since = new Date(Date.UTC(year, month - 1, 1));
-        var until = new Date(Date.UTC(year, month, 0));
-        return {
-            since: since,
-            until: until
-        };
-    }
-
-    parseData(data, headers, since, until) {
-        var rows = [];
-        if (data.items) {
-            var items = data.items;
-            items.forEach(function (item) {
-                var reportRange = parseReportDateRange(item.name);
-                var reportSince = reportRange.since;
-                var reportUntil = reportRange.until;
-                if (!(since <= reportUntil && reportSince <= until)) {
-                    // Out of range.
-                    return;
-                }
-                // console.log(Utilities.formatString(
-                //   '%s: reportName = %s since = %s until = %s',
-                //   getSessionId(),
-                //   item.name,
-                //   EE.formatDate(reportSince),
-                //   EE.formatDate(reportUntil)
-                // ));
-                // console.log(Utilities.formatString('%s: mediaLink = %s', getSessionId(), item.mediaLink));
-
-                var blob = UrlFetchApp.fetch(item.mediaLink, createOptions()).getBlob();
-                blob.setContentType('application/zip');
-                // console.log(Utilities.formatString('%s: blob size = %d', getSessionId(), blob.getBytes().length));
-
-                var unzippedBlob = Utilities.unzip(blob);
-                var unzippedData = unzippedBlob[0].getDataAsString();
-
-                var dataRows = Utilities.parseCsv(unzippedData);
-                dataRows.forEach(function (row) {
-                    var date = new Date(row[mapHeaderIndex(DIMENSION_DATE)]);
-                    if (!(since <= date && date <= until)) {
-                        // Out of range.
-                        return;
-                    }
-                    var dict = {};
-                    headers.forEach(function (header) {
-                        var value = row[mapHeaderIndex(header)];
-                        if (header == DIMENSION_DATE) {
-                            value = ee.parseDate(value);
-                        }
-                        if (header == METRIC_AMOUNT) {
-                            // Remove all commas.
-                            value = value.replace(/,/g, '');
-
-                            // Minus 30% Google charge.
-                            value = value * 0.7;
-
-                            // Multiply by conversion rate.
-                            var currency = row[9];
-                            var rate = ee.getRate(currency);
-                            value = value * rate;
-                        }
-                        dict[header] = value;
-                    });
-                    rows.push(dict);
-                });
-            });
-        }
-        return rows;
-    }
-
-    buildKey(bucketId, dimensions, metrics, since, until) {
-        var params = [];
-        params.push(getAccessToken());
-        params.push(bucketId);
-        params.extend(dimensions);
-        params.extend(metrics);
-        params.push(ee.formatDate(since));
-        params.push(ee.formatDate(until));
-        return params.join('|');
-    }
-
-    retrieveData(url) {
-        var options = createOptions();
-        var result = ee.sendHttpGET(url, options).then(res => {
-            return res.json();
-        }).then(json => {
-            return json;
+            if (header === METRIC_AMOUNT) {
+              value = value.replace(/,/g, '');
+              const discount = value * 0.7;
+              const currency = row[9];
+              const rate = await EE.getRate(currency);
+              value = value * rate * discount;
+            }
+            dict[header] = value;
+          });
+          rows.push(dict);
         });
-        var dict = JSON.parse(result);
-        return dict;
+      }));
     }
+    return rows;
+  }
 
-    requestHttp(bucketId, dimensions, metrics, since, until, useCache) {
-        var parseData = undefined;
-        lock.acquire(key, (done) => {
-            var url = formatObjectsListUrl(bucketId);
-            var data = this.retrieveData(url);
-            var headers = dimensions.concat(metrics);
-            parsedData = this.parseData(data, headers, since, until);
-            done();
-        }, (err, ret) => {
-            return JSON.parse(parseData);
-        })
-    }
+  static buildKey(token, bucketId, dimensions, metrics, since, until) {
+    let params = [];
+    params = [
+      token,
+      bucketId,
+      ...dimensions,
+      ...metrics,
+      EE.formatDate(since),
+      EE.formatDate(until),
+    ];
+    return params.join('|');
+  }
+
+  static async retrieveData(url, token) {
+    const options = this.createOption(token);
+    const response = await EE.sendHttpGET(url, options);
+    // console.log(response.error);
+    const result = await response.json();
+    return result;
+  }
+
+  static async requestHttp(token, bucketId, dimensions, metrics, since, until, useCache) {
+    let parsedData;
+    const lock = new AsyncLock({
+      timeout: LOCK_DURATION,
+    });
+    const result = await lock.acquire('key', async () => {
+      const buildKey = this.buildKey(token, bucketId, dimensions, metrics, since, until, useCache);
+      const key = EE.hash(buildKey);
+      const cache = EE.cache();
+      const value = useCache ? cache.get(key) : null;
+      if (value != null) {
+        const data = await EE.decompress(value);
+        parsedData = JSON.parse(data);
+      } else {
+        const url = this.formatObjectsListUrl(bucketId);
+        const data = await this.retrieveData(url, token);
+        const headers = dimensions.concat(metrics);
+        parsedData = await this.parseData(token, data, headers, since, until);
+        const raw = JSON.stringify(parsedData);
+        const compress = await EE.compress(raw);
+        cache.put(key, compress, CACHE_DURATION);
+      }
+      return parsedData;
+    });
+    return result;
+  }
 }
 module.exports = AndroidPublisher;
